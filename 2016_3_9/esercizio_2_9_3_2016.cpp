b9427807bfcbd63a6a04e28c2fd30b4165e9fdb0
Andrea <andrea.favero96@gmail.com>
2016-03-13 18:31:36 +0100

    Update esercizio_2_9_3_2016.cpp

--- 2016_3_9/esercizio_2_9_3_2016.cpp
+++ 2016_3_9/esercizio_2_9_3_2016.cpp
@@ -1,1 +1,26 @@
-/**/
+/*
+  Feiv
+  Osha
+  13/3/2014
+*/
+#include<iostream>
+#include<fstream>
+
+int main()
+{
+  int A[2][4][5], n_el, iletto;
+  ifstream IN("input");
+  ofstream OUT("output");
+  
+  if(IN) {
+    
+  } else
+    return -1;
+  
+  if(OUT) {
+    
+  } else
+    return -1;
+  
+  return 0;
+}
